ad865df35b9ef498f039cac6759fe4e274dfc8ad
Suman Paul <sumank.paul@gmail.com>
2014-01-16 18:52:46 +0530

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('orbApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
