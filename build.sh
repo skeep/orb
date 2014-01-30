ff38cf83d10d085de538227a8e0e13d863be0e48
Suman Paul <sumank.paul@gmail.com>
2014-01-30 13:18:42 +0530

    update build script

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ cp resources/package.json dist/package.json
 #cd dist
 #zip -r ../${PWD##*/}.nw *
 grunt build-app
+touch build/releases/Orb/mac/orb.txt
+touch build/releases/Orb/win/orb.txt
+dropbox_uploader upload build/releases/Orb/mac/orb.txt /mac/
+dropbox_uploader upload build/releases/Orb/win/orb.txt /win/
